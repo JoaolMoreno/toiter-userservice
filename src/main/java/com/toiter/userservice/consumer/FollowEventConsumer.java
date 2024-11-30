7a49f95755ebcbe18b8410fb99ab8016e3f7208c
João Pedro Lopes <jl.moreno7@hotmail.com>
2024-11-30 17:19:17 -0400

    Novo consumer para atualizar em tempo real contagem de seguidores no redis

---
+++ src/main/java/com/toiter/userservice/consumer/FollowEventConsumer.java
@@ -0,0 +1,51 @@
+package com.toiter.userservice.consumer;
+
+import com.toiter.userservice.model.FollowCreatedEvent;
+import com.toiter.userservice.model.FollowDeletedEvent;
+import com.toiter.userservice.model.UserPublicData;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Service
+public class FollowEventConsumer {
+
+    private final RedisTemplate<String, UserPublicData> redisTemplateForUserPublicData;
+    private static final String USER_PUBLIC_DATA_KEY_PREFIX = "user:public:";
+    private static final Logger logger = LoggerFactory.getLogger(FollowEventConsumer.class);
+
+    public FollowEventConsumer(RedisTemplate<String, UserPublicData> redisTemplateForUserPublicData) {
+        this.redisTemplateForUserPublicData = redisTemplateForUserPublicData;
+    }
+
+    @KafkaListener(topics = "follow-created-topic", groupId = "follow-event-consumers")
+    public void consumeFollowCreatedEvent(FollowCreatedEvent event) {
+        processFollowEvent(event.getUserId(), 1, "FollowCreatedEvent");
+    }
+
+    @KafkaListener(topics = "follow-deleted-topic", groupId = "follow-event-consumers")
+    public void consumeFollowDeletedEvent(FollowDeletedEvent event) {
+        processFollowEvent(event.getUserId(), -1, "FollowDeletedEvent");
+    }
+
+    private void processFollowEvent(Long userId, int delta, String eventType) {
+        String userPublicDataKey = USER_PUBLIC_DATA_KEY_PREFIX + userId;
+        logger.info("Processing {} for user ID: {}", eventType, userId);
+
+        if (Boolean.TRUE.equals(redisTemplateForUserPublicData.hasKey(userPublicDataKey))) {
+            UserPublicData publicData = redisTemplateForUserPublicData.opsForValue().get(userPublicDataKey);
+
+            if (publicData != null) {
+                int newFollowerCount = Math.max(0, publicData.getFollowersCount() + delta);
+                publicData.setFollowersCount(newFollowerCount);
+                redisTemplateForUserPublicData.opsForValue().set(userPublicDataKey, publicData);
+
+                logger.info("Updated followers count for user ID: {}. New count: {}", userId, newFollowerCount);
+            }
+        } else {
+            logger.warn("User ID {} not found in Redis. Skipping {}.", userId, eventType);
+        }
+    }
+}
