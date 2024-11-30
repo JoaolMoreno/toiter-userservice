4158acc1eb8f3922ad0b43f25e6e011e4e076f79
João Pedro Lopes <jl.moreno7@hotmail.com>
2024-11-30 16:31:55 -0400

    Adicionanodo serviço para criar uma imagem ou atualizar a existente

--- src/main/java/com/toiter/userservice/service/ImageService.java
+++ src/main/java/com/toiter/userservice/service/ImageService.java
@@ -3,6 +3,10 @@ package com.toiter.userservice.service;
 import com.toiter.userservice.entity.Image;
 import com.toiter.userservice.repository.ImageRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Service
 public class ImageService {
@@ -16,5 +20,22 @@ public class ImageService {
     public Image getImageById(Long id) {
         return imageRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Image not found"));
+    }
+
+    public Image updateOrCreateImage(Long imageId, MultipartFile imageFile) throws IOException {
+        byte[] imageBytes = imageFile.getBytes();
+
+        Image image = imageId != null
+                ? imageRepository.findById(imageId)
+                .orElseThrow(() -> new IllegalArgumentException("Image not found"))
+                : new Image();
+
+        image.setImage(imageBytes);
+
+        if (imageId == null) {
+            image.setCreationDate(LocalDateTime.now());
+        }
+
+        return imageRepository.save(image);
     }
 }
