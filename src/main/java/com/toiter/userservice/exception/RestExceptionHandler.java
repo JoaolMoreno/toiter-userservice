39785e590cc3db0474aafa51545446c8fa3081fb
João Pedro Lopes <jl.moreno7@hotmail.com>
2024-12-01 00:37:07 -0400

    Ativando log de erro

--- src/main/java/com/toiter/userservice/exception/RestExceptionHandler.java
+++ src/main/java/com/toiter/userservice/exception/RestExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.toiter.userservice.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
@@ -27,12 +30,13 @@ public class RestExceptionHandler {
                 errorMessage = "Username is already in use";
             }
         }
-
+        logger.error(String.valueOf(ex));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception ex) {
+        ex.printStackTrace();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred");
     }
 }
