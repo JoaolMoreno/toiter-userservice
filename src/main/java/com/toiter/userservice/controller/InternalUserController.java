94de01ae72e6efc937c13e238c967521a28ad4e7
João Pedro Lopes Moreno <jl.moreno7@hotmail.com>
2024-12-02 16:07:09 -0400

    Novo InternalUserController para solicitações internas entre serviços

---
+++ src/main/java/com/toiter/userservice/controller/InternalUserController.java
@@ -0,0 +1,22 @@
+package com.toiter.userservice.controller;
+
+import com.toiter.userservice.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/internal/users")
+public class InternalUserController {
+    private final UserService userService;
+
+    public InternalUserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{username}/id")
+    public ResponseEntity<Long> getUserId(
+            @PathVariable String username) {
+        Long userId = userService.getUserIdByUsername(username);
+        return ResponseEntity.ok(userId);
+    }
+}
