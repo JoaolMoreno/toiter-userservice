e14c3b8847b1ad9390042cfb6b9e7236d01f2886
João Pedro Lopes Moreno <jl.moreno7@hotmail.com>
2024-12-03 12:54:34 -0400

    Adicionando url no swagger

--- src/main/java/com/toiter/userservice/config/SwaggerConfig.java
+++ src/main/java/com/toiter/userservice/config/SwaggerConfig.java
@@ -5,14 +5,22 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    @Value("${server.url}")
+    private String serverUrl;
 
     @Bean
     public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl(serverUrl);
+        server.setDescription("Production Server");
+
         SecurityScheme bearerAuthScheme = new SecurityScheme()
                 .type(SecurityScheme.Type.HTTP)
                 .scheme("bearer")
@@ -20,6 +28,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("bearerAuth", bearerAuthScheme))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .info(new Info().title("API Documentation").version("1.0"));
+                .info(new Info().title("API Documentation").version("1.0"))
+                .addServersItem(server);
     }
 }
