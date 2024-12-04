cc746cda76393feb0af43a210c5c8e0cd5c5a0f8
joao.moreno <jl.moreno7@hotmail.com>
2024-12-04 12:37:52 -0400

    Liberando acesso ao metodo OPTIONS nos endpoints

--- src/main/java/com/toiter/userservice/config/SecurityConfig.java
+++ src/main/java/com/toiter/userservice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.toiter.userservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -31,6 +32,7 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html").permitAll()
                         .requestMatchers("/internal/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
