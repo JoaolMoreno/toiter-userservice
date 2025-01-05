cb0986e0b2f520441cacfea7c75e298ec54de59d
João Pedro Lopes <jl.moreno7@hotmail.com>
2025-01-05 01:11:57 -0400

    Criando client para comunicação com serviço de posts

---
+++ src/main/java/com/toiter/userservice/service/PostClientService.java
@@ -0,0 +1,38 @@
+package com.toiter.userservice.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+
+@Service
+public class PostClientService {
+
+    private static final Logger logger = LoggerFactory.getLogger(PostClientService.class);
+    private final RestTemplate restTemplate;
+
+    @Value("${SERVICE_POST_URL}")
+    private String postServiceUrl;
+
+    @Value("${service.shared-key}")
+    private String sharedKey;
+
+    public PostClientService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public Integer getPostsCount(Long userId) {
+        logger.debug("Fetching posts count for user ID: {}", userId);
+        String url = postServiceUrl + "/posts/count?userId=" + userId;
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + sharedKey);
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(url, HttpMethod.GET, entity, Integer.class).getBody();
+    }
+}
